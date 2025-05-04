7f87c34573e669ca49733609a57fd33300548013
B2002sane <bintousane69@gmail.com>
2025-05-04 08:03:40 +0000

    feat modification mineur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,14 +27,6 @@ const routes: Routes = [
         canActivate: [LoginGuard]
       },
       {
-        path: 'typography',
-        loadComponent: () => import('./demo/component/basic-component/color/color.component').then((c) => c.ColorComponent)
-      },
-      {
-        path: 'color',
-        loadComponent: () => import('./demo/component/basic-component/typography/typography.component').then((c) => c.TypographyComponent)
-      },
-      {
         path: 'sample-page',
         loadComponent: () => import('./demo/others/sample-page/sample-page.component').then((c) => c.SamplePageComponent),
         canActivate: [LoginGuard]
